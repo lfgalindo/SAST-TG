f35e544fbed95a637f13a5b0b5b168819243852c
Luís Guilherme Fernandes Ferreira <guihms1@hotmail.com>
2016-08-30 15:04:46 -0300

    Classe Cargo criada

---
+++ SAST/src/main/java/br/com/sast/domain/Cargo.java
@@ -0,0 +1,11 @@
+package br.com.sast.domain;
+
+/*
+ * Classe responsável por modelar a tabela Cargo
+ * @author Luís Guilherme Fernandes Ferreira
+ * @since Classe criada em 30/08/2016
+ */
+
+public class Cargo {
+
+}
