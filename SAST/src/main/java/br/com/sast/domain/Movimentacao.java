65d81c51079a387589136bf9bdb25b54e54a36f7
Luis Claudio G. Sanches <luis.cgs@hotmail.com>
2016-08-29 21:03:35 -0300

    Upando classes de modelo-banco (Luis C.)

---
+++ SAST/src/main/java/br/com/sast/domain/Movimentacao.java
@@ -0,0 +1,12 @@
+package br.com.sast.domain;
+
+/**
+ * Classe definida para mapear a entidade "tb_movimentacao" e seus atributos.
+ * 
+ * @author Luis Claudio Gonçalves Sanches <luis.cgs@hotmail.com>
+ * @since 29/08/2016
+ */
+
+public class Movimentacao {
+
+}
