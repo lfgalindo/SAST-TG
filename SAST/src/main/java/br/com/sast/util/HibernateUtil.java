e7e9117196a9413ba991fb438b21016da91b1cec
lfelipe95 <lfgalindo@live.com>
2016-08-31 20:01:06 -0300

    HibernateUtil Workando

--- SAST/src/main/java/br/com/sast/util/HibernateUtil.java
+++ SAST/src/main/java/br/com/sast/util/HibernateUtil.java
@@ -1,10 +1,7 @@
 package br.com.sast.util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
 
 /**
  * Classe definida para gerenciar sessões de conexão com o banco.
@@ -13,31 +10,22 @@ import org.hibernate.service.ServiceRegistry;
  */
 
 public class HibernateUtil {
-	
-	private static final SessionFactory sessionFactory = buildSessionFactory();
-   
-	private static SessionFactory buildSessionFactory(){
-		
-		try{
-			// Cria um SessionFactory de acordo com o hibernate.cfg.xml
-			Configuration configuration = new Configuration();
-			configuration.configure();
-			
-			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-					.applySettings(configuration.getProperties()).build();
-			
-			SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-			
-			return sessionFactory;
-		} 
-		catch (Throwable e){
-			// Mensagem de erro
-			System.out.println("Erro na criação da fábrica de sessão." + e.getMessage());
-			throw new ExceptionInInitializerError(); 
-		}
-	}
-		
-	public static SessionFactory getSessionFactory() {
+ 
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+ 
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            return new Configuration()
+            		.configure()
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+ 
+    public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
 } // Fim da Classe
