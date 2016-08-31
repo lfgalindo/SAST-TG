5fd7ea0748ab44b2b7f3f2fa91a1d64ca5291600
lfelipe95 <lfgalindo@live.com>
2016-08-30 23:53:27 -0300

    Adicionado pacote DAO

---
+++ SAST/src/main/java/dao/PerfilDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+/**
+ * Classe definida para realizar a persistência na entidade "tb_perfil".
+ * @author Luiz Felipe Magalhães Galindo <lfgalindo@live.com>
+ * @since 30/08/2016
+ */
+
+public class PerfilDAO {
+
+}
---
+++ SAST/src/main/java/dao/PlanoDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+/**
+ * Classe definida para realizar a persistência na entidade "tb_plano".
+ * @author Luiz Felipe Magalhães Galindo <lfgalindo@live.com>
+ * @since 30/08/2016
+ */
+
+public class PlanoDAO {
+
+}
