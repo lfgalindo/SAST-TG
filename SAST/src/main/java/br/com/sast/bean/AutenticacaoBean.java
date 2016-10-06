4014c129b1c0010c860fc66d509f025c4573d639
Luiz Felipe <lfgalindo@live.com>
2016-10-05 22:29:44 -0300

    Alterações no CSS

--- SAST/src/main/java/br/com/sast/bean/AutenticacaoBean.java
+++ SAST/src/main/java/br/com/sast/bean/AutenticacaoBean.java
@@ -3,12 +3,16 @@ package br.com.sast.bean;
 import br.com.sast.dao.FuncionarioDAO;
 import br.com.sast.domain.Funcionario;
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 /**
  * Classe definida para controlar a autenticação de Usuários
  * @author Luiz Felipe Magalhães Galindo <lfgalindo@live.com>
  * @since 04/10/2016
  */
+@ManagedBean
+@SessionScoped
 public class AutenticacaoBean {
     FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
     private Funcionario funcionario;
@@ -37,8 +41,8 @@ public class AutenticacaoBean {
 
     public void autenticar() {
         //try {
-            FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-            funcionarioLogado = funcionarioDAO.autenticar(funcionario.getLogin(), funcionario.getSenha());
+            FuncionarioDAO funcDAO = new FuncionarioDAO();
+            funcionarioLogado = funcDAO.autenticar(funcionario.getLogin(), funcionario.getSenha());
             
             //if(funcionarioLogado == null){
               //  Messages.addGlobalError("Login e/ou senha incorreta(s)");
