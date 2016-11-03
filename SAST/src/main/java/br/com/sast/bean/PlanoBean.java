fbcf78c29b5ed855ae74915e3de740a59b1c9ab6
Luís Guilherme Fernandes Ferreira <guihms1@hotmail.com>
2016-11-03 10:30:36 -0200

    Inclusão, vistualização, listagem, edição e exclusão de planos terminadas. Algumas alterações na parte de clientes

--- SAST/src/main/java/br/com/sast/bean/PlanoBean.java
+++ SAST/src/main/java/br/com/sast/bean/PlanoBean.java
@@ -1,11 +1,12 @@
 package br.com.sast.bean;
 
 /**
- * Classe definida para controlar a comunição entre a classe PlanoDAO e sua Interface Web
+ * Classe definida para controlar a comunição entre a classe PlanoDAO e sua
+ * Interface Web
+ *
  * @author Luís Guilherme Fernandes Ferreira <guihms1@gmail.com>
  * @since Classe criada em 25/10/2016
  */
-
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -15,91 +16,104 @@ import org.omnifaces.util.Messages;
 
 import br.com.sast.dao.PlanoDAO;
 import br.com.sast.domain.Plano;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.omnifaces.util.Faces;
 
 @ManagedBean
 @ViewScoped
 public class PlanoBean {
-	
-	private Plano plano;
-	private List<Plano> planos;
-	private Integer codigo;
-	
-	
-	//GETS & SETS
-	public Plano getPlano() {
-		return plano;
-	}
-	public void setPlano(Plano plano) {
-		this.plano = plano;
-	}
-	public List<Plano> getPlanos() {
-		return planos;
-	}
-	public void setPlanos(List<Plano> planos) {
-		this.planos = planos;
-	}
-	public Integer getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
-	}
-	
-	
-	//MÉTODOS CRUD
-	
-	//Método que prepara a tela para inserir novo registro
-	public void novo(){
-		
-		plano = new Plano();
-		
-	}//fim do método novo
-	
-	//método para inserir um novo registro no banco
-	public void salvar(){
-		
-		PlanoDAO planoDAO = new PlanoDAO();
-		planoDAO.inserir(plano);
-		
-		Messages.addGlobalInfo("Plano inserido com sucesso!");
-		
-		novo();
-		
-	}//fim do método salvar
-	
-	//método para listar os registros do banco.
-	public void listar(){
-		
-		PlanoDAO planoDAO = new PlanoDAO();
-		planos = planoDAO.listar();
-		
-	}//fim do método listar
-	
-	//método para buscar um registro específico no banco.
-	public void buscar(){
-		
-		PlanoDAO planoDAO = new PlanoDAO();
-		plano = planoDAO.buscar(codigo);
-		
-	}//fim do método buscar
-	
-	//método para editr um registro específico no banco.
-	public void editar(){
-		
-		PlanoDAO planoDAO = new PlanoDAO();
-		planoDAO.editar(plano);
-		
-		Messages.addGlobalInfo("Plano editado com sucesso!");
-		
-	}//fim do método editar
-	
-	//método para excluir um registro específico no banco.
-	public void excluir(){
-		
-		PlanoDAO planoDAO = new PlanoDAO();
-		planoDAO.excluir(plano);
-		
-		Messages.addGlobalInfo("Plano excluído com sucesso!");
-		
-	}//fim do método excluir.
+
+    private Plano plano;
+    private List<Plano> planos;
+    private Integer codigo;
+    
+     public void novo() {
+
+        plano = new Plano();
+
+    }//fim do método novo
+    
+    //GETS & SETS
+    public Plano getPlano() {
+        return plano;
+    }
+
+    public void setPlano(Plano plano) {
+        this.plano = plano;
+    }
+
+    public List<Plano> getPlanos() {
+        return planos;
+    }
+
+    public void setPlanos(List<Plano> planos) {
+        this.planos = planos;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    //MÉTODOS CRUD
+    //Método que prepara a tela para inserir novo registro
+
+    //método para inserir um novo registro no banco
+    public void salvar() {
+
+        PlanoDAO planoDAO = new PlanoDAO();
+        planoDAO.inserir(plano);
+
+        Messages.addGlobalInfo("Plano inserido com sucesso!");
+
+        novo();
+
+    }//fim do método salvar
+
+    //método para listar os registros do banco.
+    public void listar() {
+
+        PlanoDAO planoDAO = new PlanoDAO();
+        planos = planoDAO.listar();
+
+    }//fim do método listar
+
+    //método para buscar um registro específico no banco.
+    public void buscar() {
+
+        PlanoDAO planoDAO = new PlanoDAO();
+        plano = planoDAO.buscar(codigo);
+
+    }//fim do método buscar
+
+    //método para editr um registro específico no banco.
+    public void editar() {
+
+        PlanoDAO planoDAO = new PlanoDAO();
+        planoDAO.editar(plano);
+
+        Messages.addGlobalInfo("Plano editado com sucesso!");
+
+    }//fim do método editar
+
+    //método para excluir um registro específico no banco.
+    public void excluir() {
+
+        PlanoDAO planoDAO = new PlanoDAO();
+        planoDAO.excluir(plano);
+
+        Messages.addGlobalInfo("Plano excluído com sucesso!");
+
+        try {
+            Faces.redirect("planoListar.xhtml");
+        } catch (IOException ex) {
+            Logger.getLogger(PerfilBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }//fim do método excluir.
 }
--- SAST/src/main/java/br/com/sast/dao/PlanoDAO.java
+++ SAST/src/main/java/br/com/sast/dao/PlanoDAO.java
@@ -11,121 +11,121 @@ import br.com.sast.util.HibernateUtil;
 
 /**
  * Classe definida para realizar a persistência na entidade "tb_plano".
+ *
  * @author Luiz Felipe Magalhães Galindo <lfgalindo@live.com>
  * @since 30/08/2016
  */
-
 public class PlanoDAO {
-	
-	// Método que fará a persistência no banco para inserir um plano
-	public void inserir(Plano plano){
-		
-		Session sessao = HibernateUtil.getSessionFactory().openSession();
-	
+
+    // Método que fará a persistência no banco para inserir um plano
+    public void inserir(Plano plano) {
+
+        Session sessao = HibernateUtil.getSessionFactory().openSession();
+
         try {
-        	sessao.beginTransaction();
+            sessao.beginTransaction();
 
             sessao.save(plano);
 
             sessao.getTransaction().commit();
 
         } catch (RuntimeException erro) {
-        	sessao.getTransaction().rollback();
+            sessao.getTransaction().rollback();
             erro.getMessage();
-            
+
         } finally {
             sessao.close();
         }
-				
-	} // Fim do método inserir
-	
-	// Método que fará a persistência no banco para listar todos os planos armazenados
-	@SuppressWarnings({ "deprecation", "unchecked" })
-	public List<Plano> listar(){
-		
+
+    } // Fim do método inserir
+
+    // Método que fará a persistência no banco para listar todos os planos armazenados
+    @SuppressWarnings({"deprecation", "unchecked"})
+    public List<Plano> listar() {
+
         Session sessao = HibernateUtil.getSessionFactory().openSession();
-        
-        try{
+
+        try {
             Criteria consulta = sessao.createCriteria(Plano.class);
-            
+
             consulta.addOrder(Order.asc("codigo"));
-            
+
             List<Plano> resultado = consulta.list();
-            
+
             return resultado;
-            
-        }catch(RuntimeException erro){
+
+        } catch (RuntimeException erro) {
             throw erro;
-            
-        } finally{
+
+        } finally {
             sessao.close();
-            
+
         }
     } // Fim do método listar
-	
-	// Método que fará a persistência no banco para buscar um determinado plano
-	@SuppressWarnings("deprecation")
-	public Plano buscar(Integer codigo){
-		
+
+    // Método que fará a persistência no banco para buscar um determinado plano
+    @SuppressWarnings("deprecation")
+    public Plano buscar(Integer codigo) {
+
         Session sessao = HibernateUtil.getSessionFactory().openSession();
-        
-        try{
+
+        try {
             Criteria consulta = sessao.createCriteria(Plano.class);
-            
+
             consulta.add(Restrictions.eq("codigo", codigo));
 
-            Plano resultado = (Plano)consulta.uniqueResult();
+            Plano resultado = (Plano) consulta.uniqueResult();
 
             return resultado;
-            
-        }catch(RuntimeException erro){
+
+        } catch (RuntimeException erro) {
             throw erro;
-            
-        } finally{
+
+        } finally {
             sessao.close();
-            
+
         }
     } // Fim do método buscar
-    
-	// Método que fará a persistência no banco para excluir um plano
-    public void excluir(Plano plano){
-    	
+
+    // Método que fará a persistência no banco para excluir um plano
+    public void excluir(Plano plano) {
+
         Session sessao = HibernateUtil.getSessionFactory().openSession();
-                
-        try{
+
+        try {
             sessao.beginTransaction();
-            
+
             sessao.delete(plano);
-            
+
             sessao.getTransaction().commit();
-            
-        }catch(RuntimeException erro){
-        	sessao.getTransaction().rollback();
+
+        } catch (RuntimeException erro) {
+            sessao.getTransaction().rollback();
             erro.getMessage();
 
-        } finally{
+        } finally {
             sessao.close();
-        }       
+        }
     } // Fim do método Excluir
-    
+
     // Método que fará a persistência no banco para editar um plano
-    public void editar(Plano plano){
-    	
+    public void editar(Plano plano) {
+
         Session sessao = HibernateUtil.getSessionFactory().openSession();
-        
-        try{
+
+        try {
             sessao.beginTransaction();
-            
+
             sessao.update(plano);
-            
+
             sessao.getTransaction().commit();
-            
-        }catch(RuntimeException erro){
-                sessao.getTransaction().rollback();
-                erro.getMessage();
-                
-        } finally{
+
+        } catch (RuntimeException erro) {
+            sessao.getTransaction().rollback();
+            erro.getMessage();
+
+        } finally {
             sessao.close();
-        }       
+        }
     } // Fim do método Editar
 }//Fim da Classe
