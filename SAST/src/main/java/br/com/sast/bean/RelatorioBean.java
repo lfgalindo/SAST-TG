2f64b85b3d49bcf4ecad1368f2484925b27ec85b
Luis Claudio G. Sanches <luis.cgs@hotmail.com>
2016-11-15 18:36:23 -0200

    relatório

--- SAST/src/main/java/br/com/sast/bean/RelatorioBean.java
+++ SAST/src/main/java/br/com/sast/bean/RelatorioBean.java
@@ -14,7 +14,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -25,29 +27,26 @@ import org.omnifaces.util.Messages;
 
 @ManagedBean
 @ViewScoped
-public class RelatorioBean implements Serializable{
+public class RelatorioBean {
     
-    private static final long serialVersionUID = 1L;
     private static final String PAGINA_PRINCIPAL = "principal.xhtml";
     
     private List<Cliente> listaClientes;
     private List<Funcionario> listaFuncionarios;
     
-    
-    private String cidade;
-    private String cliente;
-    private String funcionario;
+    private Cliente cliente;
+    private Funcionario funcionario;
     private Date dataAgendamento;
     
     public void novo(){
         ClienteDAO clienteDAO = new ClienteDAO();
         FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
         
-        setListaClientes(clienteDAO.listar());
-        setListaFuncionarios(funcionarioDAO.listar());
-        setCidade("");
-        setCliente("");
-        setFuncionario("");
+        listaClientes = clienteDAO.listar();
+        listaFuncionarios = funcionarioDAO.listar();
+        
+        cliente = new Cliente();
+        funcionario = new Funcionario();
     }
     
     public void imprimir() throws IOException{
@@ -55,20 +54,20 @@ public class RelatorioBean implements Serializable{
             String caminho = Faces.getRealPath("/relatorios/jasper/Manutencoes.jasper");
             Map<String, Object> parametros = new HashMap();
             
-            if(Util.isNotNull(getCliente())){
-                parametros.put("MANUTENCAO_CLIENTE", "%" + getCliente() + "%");
+            if(Util.isNotNull(getCliente().getNome())){
+                parametros.put("MANUTENCAO_CLIENTE", "%" + this.cliente + "%");
             } else {
                 parametros.put("MANUTENCAO_CLIENTE", "%%");
             }
             
-            if(Util.isNotNull(getFuncionario())){
-                parametros.put("MANUTENCAO_FUNCIONARIO","%" + getFuncionario()+ "%");
+            if(Util.isNotNull(getFuncionario().getNome())){
+                parametros.put("MANUTENCAO_FUNCIONARIO","%" + getFuncionario() + "%");
             } else {
                 parametros.put("MANUTENCAO_FUNCIONARIO", "%%");
             }
             
-            if(Util.isNotBlank(getCidade())){
-                parametros.put("MANUTENCAO_CIDADE","%" + getCidade() + "%");
+            if(Util.isNotBlank(getCliente().getCidade())){
+                parametros.put("MANUTENCAO_CIDADE","%" + getCliente().getCidade() + "%");
             } else {
                 parametros.put("MANUTENCAO_CIDADE", "%%");
             }
@@ -80,6 +79,7 @@ public class RelatorioBean implements Serializable{
             JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
             JasperPrintManager.printReport(relatorio, true);
             
+            Messages.addGlobalInfo("Relatório gerado com sucesso!");
         } catch (JRException erro) {
             Messages.addGlobalError("Ocorreu um erro na geração do relatório ");
             erro.printStackTrace();
@@ -102,14 +102,6 @@ public class RelatorioBean implements Serializable{
         this.listaFuncionarios = listaFuncionarios;
     }
 
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
     public Date getDataAgendamento() {
         return dataAgendamento;
     }
@@ -118,21 +110,20 @@ public class RelatorioBean implements Serializable{
         this.dataAgendamento = dataAgendamento;
     }
 
-    public String getCliente() {
+    public Cliente getCliente() {
         return cliente;
     }
 
-    public void setCliente(String cliente) {
+    public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
 
-    public String getFuncionario() {
+    public Funcionario getFuncionario() {
         return funcionario;
     }
 
-    public void setFuncionario(String funcionario) {
+    public void setFuncionario(Funcionario funcionario) {
         this.funcionario = funcionario;
     }
-    
-    
+
 }
